1e70f9dfa0fac59458554a4ec59de24543fc414b
Leehutsonjr <58994542+Leehutsonjr@users.noreply.github.com>
2022-04-14 12:04:10 -0400

    Add files via upload

---
+++ ScriptMaker1/MainWindow.xaml.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScriptMaker1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SingleInsert_Click(object sender, RoutedEventArgs e) //This will put each complete row in an insert 
+        {
+            Util util = new Util();
+            List<string> needQuotes = util.DoesIndexNeedQuotes(txtBool2.Text);
+            string singleInsert = "";
+            string finalInsert = "";
+            string inputText = txtInput.Text;
+            var reader = new StringReader(inputText);
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                //If the data type requires quotations, then add
+                if (needQuotes[0].Equals("Y"))
+                {
+                    singleInsert += util.Quotes(line) + ", ";
+                }
+                else
+                {
+                    singleInsert += line + ", ";
+                }
+            }
+            //Remove the last comma before parenthesis
+            singleInsert = singleInsert.Remove(singleInsert.Length - 2, 1);
+
+            finalInsert = util.Parenthesis(singleInsert);
+
+            txtOutput.Text = finalInsert;
+        }
+
+        private void MultiInsert_Click(object sender, RoutedEventArgs e) //This will set up insert based on indexes (commas)
+        {
+            Util util = new Util();
+            List<string> needQuotes = util.DoesIndexNeedQuotes(txtBool.Text);
+            bool quote = false;
+            string singleInsert = "";
+            string multiInsert = "";
+            string finalInsert = "";
+            string inputText = txtInput.Text;
+            var reader = new StringReader(inputText);
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                //Initialize variables each time
+                int count = 0;
+                string temp = "";
+
+                //Split comma separated items 
+                //var values = Regex.Split(line, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                var values = line.Split(' ');
+
+                //Get the length of values
+
+
+                //Take each value, determine the datatype, and add quotes if necessary
+                foreach ( var v in values)
+                {
+
+                    //If the data type requires quotations, then add
+                    if(needQuotes[count].Equals("Y"))
+                    {
+                        temp += util.Quotes(v) + ", ";
+                    }
+                    else
+                    {
+                        temp += v + ", ";
+                    }
+                    count++;
+                }
+                //Remove the last comma in the string
+                temp = temp.Remove(temp.Length - 2, 1);
+
+                //add parenthesis to insert item
+                multiInsert += util.Parenthesis(temp) + ", \n";
+            }
+            //Collect all inserts for a final string with everything
+            finalInsert = multiInsert;
+
+            //Remove the last comma from the string
+            finalInsert = finalInsert.Remove(finalInsert.Length - 3, 1);
+
+            //Output to the app screen for user to copy for insert purposes
+            txtOutput.Text = finalInsert;
+        }
+
+        private void Split_Button_Click(object sender, RoutedEventArgs e) //This splits by the denoted space (space 3 for instance. It isolates the word)
+        {
+            Util util = new Util();
+            string split = "";
+            string finalInsert = "";
+            string inputText = txtInput.Text;
+            var reader = new StringReader(inputText);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                //var line = reader.ReadLine().Split('\r');
+                var values = line.Split(' ');
+
+                split += values[2] + "\r ";
+            }
+
+            //finalInsert = util.Parenthesis(singleInsert);
+
+            txtOutput.Text = split;
+        }
+
+        private void List_Button_Click(object sender, RoutedEventArgs e) //this lists by spaces
+        {
+            Util util = new Util();
+            List<string> chapters = new List<string>();
+            List<Chapters> finalChap = new List<Chapters>();
+            
+            string split = "";
+            string finalInsert = "";
+            string inputText = txtInput.Text;
+            var reader = new StringReader(inputText);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var values = line.Split(' ');
+                //var line = reader.ReadLine().Split('\r');
+                for (int i = 0; i < values.Length; i++)
+                {
+                    chapters.Add(values[i]);
+                }
+                //split += values[2] + "\r ";
+            }
+
+            foreach (var c in chapters)
+            {
+                Chapters chapList = new Chapters();
+                for (int i = 1; i <= int.Parse(c); i++)
+                {
+                    chapList.chap.Add(i.ToString());
+                }
+                finalChap.Add(chapList);
+            }
+
+            //finalInsert = util.Parenthesis(singleInsert);
+
+            txtOutput.Text = split;
+        }
+
+        private void Clear_Button_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Text = "";
+            txtOutput.Text = "";
+            txtBool.Text = "";
+            txtBool2.Text = "";
+        }
+    }
+
+    public class Chapters
+    {
+        public List<string> chap = new List<string>();
+    }
+
+}
---
+++ ScriptMaker1/Util.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptMaker1
+{
+    class Util
+    {
+        public string Quotes(string value)
+        {
+            string quote = "\'" + value + "\'";
+            return quote;
+        }
+
+        public string Parenthesis(string value)
+        {
+            string parenthesis = "";
+
+            //Check for comma at the end
+            string substr = value.Substring(value.Length - 1);
+
+            //if there is a comma, remove it
+            if(substr == ",")
+            {
+                value.Remove(value.Length - 1);
+                parenthesis = "(" + value + ")";
+            }
+            else
+            {
+                parenthesis = "(" + value + ")";
+            } 
+            return parenthesis;
+        }
+
+        public List<string> DoesIndexNeedQuotes(string YesOrNo)
+        {
+            List<string> needQuotes = new List<string>();
+            string[] temp = YesOrNo.Split(',');
+            foreach(var v in temp)
+            {
+                needQuotes.Add(v.ToUpper());
+            }
+            return needQuotes;
+        }
+    }
+}
